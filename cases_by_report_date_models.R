318c47009f117b1a8101425cf667566f5f950af7
comicmuse <colm.linehan@gmail.com>
2021-02-06 23:56:12 +0000

    Simplify cases by report date

--- cases_by_report_date_models.R
+++ cases_by_report_date_models.R
@@ -8,43 +8,15 @@ casesbydate<- zoo(cases$newCasesByPublishDate, order.by=as.Date(cases$date))
 
 avecases<-rollmean(casesbydate, 7,align="right")
 
-elbow=14
-
-
 allsince11<- avecases[index(avecases) >=as.Date("2021-01-11")]
 
-firsttrend<-head(allsince11, elbow)
-
-recent<-tail(allsince11, length(allsince11)-elbow)
+model<-lm(log(allsince11) ~ as.numeric(time(allsince11)))
 
 
-model0<-lm(log(allsince11) ~ as.numeric(time(allsince11)))
+predictline=zoo(exp(predict(model, list(as.numeric(time(allsince11))))), order.by=time(allsince11))
 
+finalmerge=merge(allsince11, predictline)
 
-model1<-lm(log(firsttrend) ~ as.numeric(time(firsttrend)))
+plot(finalmerge, plot.type="single", log="y", lty=c(1,1), type=c("b","l"), col=c("blue","red"), main=paste("7-day Ave Reported date - ", end ( recent, 1)), xlab="Date", ylab="7-day Ave New Cases")
 
-model2<-lm(log(recent) ~ as.numeric(time(recent)))
-
-
-predictline0=zoo(exp(predict(model0, list(as.numeric(time(allsince11))))), order.by=time(allsince11))
-
-predictline1=zoo(exp(predict(model1, list(as.numeric(time(firsttrend))))), order.by=time(firsttrend))
-
-predictline2=zoo(exp(predict(model2, list(as.numeric(time(recent))))), order.by=time(recent))
-
-finalmerge=merge(firsttrend, recent)
-finalmerge=merge(finalmerge, predictline1)
-finalmerge=merge(finalmerge, predictline2)
-finalmerge=merge(finalmerge, predictline0)
-
-
-
-plot(finalmerge, plot.type="single", log="y", lty=c(1,1), type=c("b","b","l","l", "l"), col=c("blue","blue","red", "red", "orange"), main="7-day Ave Reported date", xlab="Date", ylab="7-day Ave New Cases")
-
-
-
-tail ( recent, 1)
-
-summary(model0)$r.squared
-summary(model1)$r.squared
-summary(model2)$r.squared
+legend("topright", paste("R-Squared: ", round(summary(model)$r.squared, 4)))
