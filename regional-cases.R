dd44a1e39510180bf1a35b69072006fee5313e2d
comicmuse <colm.linehan@gmail.com>
2021-02-09 22:00:21 +0000

    switch regional cases to use ggplot

--- regional-cases.R
+++ regional-cases.R
@@ -2,10 +2,10 @@ library(curl)
 library(zoo)
 library(tidyr)
 library(dplyr)
+library(ggplot2)
 
 
-
-cases<-read.csv(curl("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=region;date>2021-01-04&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesByPublishDate%22:%22newCasesByPublishDate%22,%22cumCasesByPublishDate%22:%22cumCasesByPublishDate%22%7D&format=csv"), header=TRUE, stringsAsFactors=FALSE)
+cases<-read.csv(curl("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=region;date>2020-12-01&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesByPublishDate%22:%22newCasesByPublishDate%22,%22cumCasesByPublishDate%22:%22cumCasesByPublishDate%22%7D&format=csv"), header=TRUE, stringsAsFactors=FALSE)
 
 casesbyregion<- cases %>% select (-areaCode, -areaType, -cumCasesByPublishDate) %>% pivot_wider(names_from = areaName, values_from=newCasesByPublishDate, values_fn=mean)
 dates=casesbyregion$date
@@ -15,12 +15,7 @@ drawingzoo=zoo(casesbyregion, order.by=as.Date(dates))
 
 drawingzoo=rollmean(drawingzoo, 7, align=c("right"))
 
-areas=colnames(casesbyregion)
-jColors <- c('chartreuse2', 'cornflowerblue', 'darkgoldenrod1', 'peachpuff3',
-             'mediumorchid2', 'turquoise3', 'wheat4', 'slategray2', 'red')
+autoplot.zoo(drawingzoo, facets=NULL)+geom_smooth(se=F) + scale_y_continuous(trans='log10') + 
+labs(title="English Regions 7-day Ave New Cases", x="Date", y="Cases (log scale)") 
 
 
-
-plot.zoo(drawingzoo, plot.type="single", log="y", col=jColors, lwd=2,  main="7-day Average of new cases - English Regions", ylab="Cases - Log scale", xlab="Date")
-grid(equilogs=F)
-legend("topright", areas, col=jColors, lwd=2, lty=c(1, 1, 1, 1, 1, 1, 1), cex=.8, bg="white")
