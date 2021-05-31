e06f8bcb08958050a31a8894e561de72b9bd35a2
comicmuse <colm.linehan@gmail.com>
2021-05-31 11:59:03 +0100

    trend update

--- cases_by_report_date_models.R
+++ cases_by_report_date_models.R
@@ -8,21 +8,22 @@ casesbydate<- zoo(cases$newCasesByPublishDate, order.by=as.Date(cases$date))
 
 avecases<-rollmean(casesbydate, 7,align="right")
 
-allcases<- avecases[index(avecases) >=as.Date("2021-01-18")]
+allcases<- avecases[index(avecases) >=as.Date("2021-02-01")]
 
-trend_start_date=as.Date("2021-04-13")
+trend_start_date=as.Date("2021-05-20")
 
 model<-lm(log(allcases[index(allcases) >= trend_start_date]) ~ as.numeric(time(allcases[index(allcases) >= trend_start_date])))
 
-model_slope=round(100 * (1-exp(predict(model))[2]/exp(predict(model))[1]),2)
+model_slope=round(100 * (1-exp(predict(model))[1]/exp(predict(model))[2]),2)
 
 
 ggplot( mapping=aes(x = time(allcases), y = coredata(allcases)) ) + 
 scale_y_log10() + geom_point(col="red",shape=1, fill="white", size=3) + 
+#geom_line() +
 geom_smooth(method="lm", mapping=aes(x = time(allcases[index(allcases) >= trend_start_date]), y = coredata(allcases[index(allcases) >=trend_start_date])) , level=.99)+
-labs(x="date", y="cases (log scale)", title="7-day average of New Cases")+
-annotate("text",label = paste("R-Squared: ",round(summary(model)$r.squared*100, 3), "%"), y=10000, x=as.Date("2021-03-15"))+
-annotate("text",label = paste("Trend Fitted from: ",trend_start_date), y=12000, x=as.Date("2021-03-15"))+
-annotate("text",label = paste("Daily fall: ",model_slope, "%"), y=11000, x=as.Date("2021-03-15"))
+labs(x="date", y="cases (log scale)", title="7-day average of New UK Cases")+
+annotate("text",label = paste("R-Squared: ",round(summary(model)$r.squared*100, 3), "%"), y=10000, x=as.Date("2021-05-01"))+
+annotate("text",label = paste("Trend Fitted from: ",trend_start_date), y=12000, x=as.Date("2021-05-01"))+
+annotate("text",label = paste("Daily growth: ",model_slope, "%"), y=11000, x=as.Date("2021-05-01"))
 
 
