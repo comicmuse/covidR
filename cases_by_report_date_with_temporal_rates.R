9da6de396bb08513a3f168763498333f472bf524
comicmuse <colm.linehan@gmail.com>
2021-06-13 21:40:33 +0100

    Add specimen date report for regions

---
+++ cases_by_report_date_with_temporal_rates.R
@@ -0,0 +1,29 @@
+library (curl)
+library (zoo)
+library(ggplot2)
+
+cases<- read.csv(curl("https://coronavirus.data.gov.uk/api/v1/data?filters=areaName=United%2520Kingdom;areaType=overview&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesByPublishDate%22:%22newCasesByPublishDate%22,%22cumCasesByPublishDate%22:%22cumCasesByPublishDate%22%7D&format=csv"), header=TRUE, stringsAsFactors=FALSE)
+
+casesbydate<- zoo(cases$newCasesByPublishDate, order.by=as.Date(cases$date))
+
+avecases<-rollmean(casesbydate, 7,align="right")
+
+allcases<- avecases[index(avecases) >=as.Date("2020-06-01")]
+
+trend_start_date=as.Date("2021-05-20")
+
+model<-lm(log(allcases[index(allcases) >= trend_start_date]) ~ as.numeric(time(allcases[index(allcases) >= trend_start_date])))
+
+model_slope=round(100 * (1-exp(predict(model))[1]/exp(predict(model))[2]),2)
+
+
+ggplot( mapping=aes(x = time(allcases), y = coredata(allcases)) ) + 
+scale_y_log10() + #geom_point(col="red",shape=1, fill="white", size=3) + 
+geom_line() +
+geom_smooth(method="lm", mapping=aes(x = time(allcases[index(allcases) >= trend_start_date]), y = coredata(allcases[index(allcases) >=trend_start_date])) , level=.99)+
+labs(x="date", y="cases (log scale)", title="7-day average of New UK Cases")+
+annotate("text",label = paste("R-Squared: ",round(summary(model)$r.squared*100, 3), "%"), y=10000, x=as.Date("2021-05-01"))+
+annotate("text",label = paste("Trend Fitted from: ",trend_start_date), y=12000, x=as.Date("2021-05-01"))+
+annotate("text",label = paste("Daily growth: ",model_slope, "%"), y=11000, x=as.Date("2021-05-01"))
+
+
---
+++ regional-cases-per-million-specimen-date-with-pan-regional-stuff.R
@@ -0,0 +1,84 @@
+library(curl)
+library(zoo)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ragg)
+
+cases<-read.csv(curl("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=region;date>2021-01-15&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=csv"), header=TRUE, stringsAsFactors=FALSE)
+nations<-read.csv(curl("https://coronavirus.data.gov.uk/api/v1/data?filters=areaType=nation;date>2021-01-15&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newCasesBySpecimenDate%22:%22newCasesBySpecimenDate%22,%22cumCasesBySpecimenDate%22:%22cumCasesBySpecimenDate%22%7D&format=csv"), header=TRUE, stringsAsFactors=FALSE)
+
+casesbyregion<- cases %>% select (-areaCode, -areaType, -cumCasesBySpecimenDate) %>% pivot_wider(names_from = areaName, values_from=newCasesBySpecimenDate, values_fn=mean)
+casesbynation<- nations %>% select (-areaCode, -areaType, -cumCasesBySpecimenDate) %>% pivot_wider(names_from = areaName, values_from=newCasesBySpecimeDate, values_fn=mean)
+
+casesbyregion <- casesbyregion %>%
+inner_join (casesbynation)
+
+dates=casesbyregion$date
+casesbyregion$date<-NULL
+
+drawingzoo=zoo(casesbyregion, order.by=as.Date(dates))
+
+drawingzoo=rollmean(drawingzoo, 7, align=c("right"))
+
+drawingzoo$"East Midlands" <- drawingzoo$"East Midlands" / 4.8
+drawingzoo$"East of England" <- drawingzoo$"East of England" / 6.2
+drawingzoo$"London" <- drawingzoo$"London" / 8.8
+drawingzoo$"North East" <- drawingzoo$"North East" / 2.6
+drawingzoo$"North West" <- drawingzoo$"North West" / 7.3
+drawingzoo$"South East" <- drawingzoo$"South East" / 9.1
+drawingzoo$"South West" <- drawingzoo$"South West" / 5.6
+drawingzoo$"West Midlands" <- drawingzoo$"West Midlands" / 5.9
+drawingzoo$"Yorkshire and The Humber" <- drawingzoo$"Yorkshire and The Humber" / 5.4
+drawingzoo$Scotland<- drawingzoo$Scotland /5.45
+drawingzoo$Wales<- drawingzoo$Wales /3.13
+drawingzoo$"Northern Ireland"<- drawingzoo$"Northern Ireland"/1.885
+drawingzoo$England <-NULL
+
+agg_tiff("Outputs/CaseTrendRegions.tiff", units="in", width=8, height=7, res=800)
+autoplot.zoo(drawingzoo, facets=NULL)  + scale_y_continuous(trans='log10') + #geom_smooth(method="lm", se=F) + 
+labs(title="Nations and English Regions 7-day Ave New Cases/million", x="Date", y="Cases/million (log scale)")
+dev.off()
+
+
+
+# "East Midlands" 4.8
+# "East of England" 6.2
+# "London" 8.8
+# "North East" 2.6
+# "North West" 7.3
+# "South East" 9.1
+# "South West" 5.6 
+# "West Midlands" 5.9
+# "Yorkshire and The Humber" 5.4
+
+
+
+
+southzoo=merge(London=drawingzoo$London, "South East"=drawingzoo$"South East", "South West"=drawingzoo$"South West", "East of England"=drawingzoo$"East of England", Wales=drawingzoo$Wales)
+northzoo=merge("North East"=drawingzoo$"North East", "North West"=drawingzoo$"North West", "East Midlands"=drawingzoo$"East Midlands", "West Midlands"=drawingzoo$"West Midlands", "Yorkshire and The Humber"=drawingzoo$"Yorkshire and The Humber")
+celticnorthzoo=merge(Scotland=drawingzoo$Scotland, "Northern Ireland"=drawingzoo$"Northern Ireland")
+dev.off()
+
+agg_tiff("Outputs/CaseTrendNorth.tiff", units="in", width=8, height=7, res=800)
+autoplot.zoo(northzoo, facets=NULL) + scale_y_continuous(trans='log10', limits=c(10,1000) ) + 
+labs(title="The North (Specimen Date)", x="Date", y="Cases/million (log scale)")
+#+
+# geom_smooth(method="lm", se=F, lwy=1) 
+dev.off()
+
+
+agg_tiff("Outputs/CaseTrendSouth.tiff", units="in", width=8, height=7, res=800)
+autoplot.zoo(southzoo, facets=NULL) + scale_y_continuous(trans='log10', limits=c(10,1000)) + 
+labs(title="The South (Specimen Date)", x="Date", y="Cases/million (log scale)")
+#+
+# geom_smooth(method="lm", se=F, lwy=1) 
+dev.off()
+
+
+agg_tiff("Outputs/CaseTrendCelticNorth.tiff", units="in", width=8, height=7, res=800)
+autoplot.zoo(celticnorthzoo, facets=NULL) + scale_y_continuous(trans='log10', limits=c(10,1000)) + 
+labs(title="Scotland/NI (Specimen Date)", x="Date", y="Cases/million (log scale)")+
+ geom_smooth(method="lm", se=F, lwy=1) 
+dev.off()
+
